7f358b6eca173329258a412e3feb6b81621ae435
ksx1777 <70083608+ksx1777@users.noreply.github.com>
2021-12-05 23:23:50 -0300

    Add files via upload

---
+++ src/main.c
@@ -0,0 +1,8 @@
+#include "flappy_bird.h"
+
+
+
+int main(void){
+
+	return 0;
+}
---
+++ src/variables.h
@@ -0,0 +1,17 @@
+#include "flappy_bird.h"
+
+typedef unsigned char byte;
+
+/* tipos definidios para string */
+typedef byte* uString;
+typedef char* String;
+
+/* tipos definidos para inteiro */
+
+// unsigned
+typedef unsigned short uint16;
+typedef unsigned int   uint32;
+
+// signed
+typedef short int16;
+typedef int   int32;
